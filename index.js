0aa700acdf11f319cd8902c75c19c1a14ae25134
DineshRamaraj <dineshkumar26032001@gmail.com>
2023-12-04 08:16:58 +0000

    react JSX

---
+++ index.js
@@ -0,0 +1,12 @@
+const element = (
+  //   Write your code here.
+  <div className="background">
+    <h1 className="heading">Super Over League</h1>
+    <div className="background-image">
+      <img src="https://assets.ccbp.in/frontend/react-js/rcb-img.png" />
+      <img src="https://assets.ccbp.in/frontend/react-js/csk-img.png" />
+    </div>
+  </div>
+);
+
+ReactDOM.render(element, document.getElementById("root"));
